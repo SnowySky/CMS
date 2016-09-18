7fccfff5192b27f2ea275c900cb586d41043f0eb
SckottJackson <deviaterikov@gmail.com>
2016-09-19 00:02:08 +0500

    f

---
+++ boot-test.ru/www/index.php
@@ -0,0 +1,74 @@
+﻿
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+    <link href="protected/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/themes/style.css" rel = "stylesheet">
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href = "#">Проекты</a></li>
+                <li><a href = "#">Преимущества</a></li>
+                <li><a href = "#">Цены</a></li>
+                <li><a href = "#">Оформить заказ</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="slider">
+</div>
+
+<div class="main-content-block">
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 block-title big-margin">
+            Проекты
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 block-description middle-margin">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+        </div>
+        <div class="col-md-7 col-lg-7 project-example"></div>
+        <div class="cols-xs-12 cols-sm-1 cols-md-1 col-lg-1"></div>
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 project-example"></div>
+        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 project-example"></div>
+        <div class="cols-xs-12 cols-sm-1 cols-md-1 col-lg-1"></div><div class="col-xs-12 col-sm-7 col-md-7 col-lg-7 project-example"></div>
+    </div>
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 block-title big-margin">
+            Преимущества
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 block-description middle-margin">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.
+        </div>
+    </div>
+    <div class="container">
+        123123
+    </div>
+</div>
+
+
+<script src="protected/js/jquery-3.1.0.min.js"></script>
+<script src="protected/js/bootstrap.min.js"></script>
+</body>
+</html>
