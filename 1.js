9799c809dca5b06422d66a5eab4226360ef63ec4
Snowy <snowyman@inbox.ru>
2016-09-19 19:31:59 +0500

    global update

---
+++ 1.js
@@ -0,0 +1,11 @@
+<script id="bx24_form_inline" data-skip-moving="true">
+    (function(w,d,u,b){
+        w['Bitrix24FormObject']=b;w[b] = w[b] || function(){arguments[0].ref=u;
+            (w[b].forms=w[b].forms||[]).push(arguments[0])};
+        if(w[b]['forms']) return;
+        s=d.createElement('script');r=1*new Date();s.async=1;s.src=u+'?'+r;
+        h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+    })(window,document,'https://integralmedia.bitrix24.ru/bitrix/js/crm/form_..','b24form');
+
+b24form({"id":"6","lang":"ru","sec":"ymdfo1","type":"inline"});
+</script>
